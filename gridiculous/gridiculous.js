6bc5a3b6cbcfb8ffe37ee76ed24d48480c89c401
dreamForge <54947833+lukeplays33@users.noreply.github.com>
2025-08-22 08:13:34 +0000

    added a init and test play ground

--- gridiculous/gridiculous.js
+++ gridiculous/gridiculous.js
@@ -1,4 +1,4 @@
-class Gridiculous {
+export class Gridiculous {
     initialize () {
         addCSSSheets('https://raw.githubusercontent.com/dreamForge-forging-our-dreams-in-tech/Pichai-UX---Gridiculous/refs/heads/main/gridiculous/css/style.css', 'GridiculousCSS');
     }
